9482ea9e36c5f90e3561b0cc255d74f0d9d9cd57
c9s <cornelius.howl@gmail.com>
2013-02-20 00:32:49 +0800

    guid can be omit

--- rss/item.go
+++ rss/item.go
@@ -5,7 +5,7 @@ type Item struct {
 	Link        string        `xml:"link,omitempty"`
 	Comments    string        `xml:"comments,omitempty"`
 	PubDate     Date          `xml:"pubDate"`
-	GUID        string        `xml:"guid"`
+	GUID        string        `xml:"guid,omitempty"`
 	Category    []string      `xml:"category,omitempty"`
 	Enclosure   ItemEnclosure `xml:"enclosure"`
 	Description string        `xml:"description"`
